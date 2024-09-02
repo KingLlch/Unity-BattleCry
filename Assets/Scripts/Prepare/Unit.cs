cb68c323ee8aec71fe6009a1deccdf8346698482
Arthur <76882612+KingLlch@users.noreply.github.com>
2024-09-02 21:34:08 +0300

    Unit

---
+++ Assets/Scripts/Prepare/Unit.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public int MaxHealth;
+    public int Health;
+
+    public int PierceDamage;
+    public int SlashDamage;
+    public int BluntDamage;
+
+    public int FireDamage;
+    public int IceDamage;
+    public int EarthDamage;
+    public int WaterDamage;
+
+    public int LightDamage;
+    public int DarknessDamage;
+}
